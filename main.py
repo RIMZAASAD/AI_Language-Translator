aff55a792b823cc365032ba78822bed705cba6e3
RIMZAASAD <162448882+RIMZAASAD@users.noreply.github.com>
2025-06-17 20:54:36 +0500

    Updated: removed dotenv and used streamlit secrets

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
+from openai import OpenAI
 import google.generativeai as genai
 
-# Load environment variables
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = st.secrets["GEMINI_API_KEY"]
 
 # Configure Gemini API
 genai.configure(api_key=api_key)
@@ -19,18 +18,16 @@ languages = [
     "Greek", "Turkish", "Hebrew", "Chinese (Simplified)", "Chinese (Traditional)", "Japanese", "Korean",
     "Thai", "Vietnamese", "Indonesian", "Filipino", "Malay", "Swahili", "Zulu", "Xhosa"
 ]
-
-# Streamlit UI
 st.set_page_config(page_title="Translator by Rimza", layout="centered")
 st.title("AI Languages Translator 🌐")
 st.write("Created by **Rimza Asad** – Translate your English text into various languages using Gemini AI.")
 
-# User Inputs
+
 text = st.text_area("Enter text in English", height=200, placeholder="Type your text here...")
 lang = st.selectbox("Select Language", languages)
 btn = st.button("Translate")
 
-# Translation Logic
+
 if btn and text:
     try:
         model = genai.GenerativeModel("gemini-1.5-flash")
@@ -42,4 +39,3 @@ if btn and text:
 
     except Exception as e:
         st.error(f"❌ Error: {str(e)}")
-
